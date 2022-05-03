ffb8e5686ecf72c3c20d93f0122cb1bc16d8971f
ienHua <s0912253128@gmail.com>
2022-05-03 16:16:21 +0800

    add Math Branch

--- GitBranchSample/src/main/java/Second.java
+++ GitBranchSample/src/main/java/Second.java
@@ -2,5 +2,13 @@
 public class Second {
 	public static void main(String[] args) {
 		System.out.println("2nd version");
+		Math cMath = new Math();
+		System.out.println(cMath.add(1, 2));
+	}
+}
+
+class Math {
+	int add(int a, int b) {
+		return a + b;
 	}
 }
